dcecc68c006bf9a8decb7cc973d6e0cdb66add9f
Michael Witkovsky <michael.witkovsky@gmail.com>
2017-12-21 23:13:43 -0500

    day 13 optimization 2

--- Day 13/packet_scanners.py
+++ Day 13/packet_scanners.py
@@ -8,48 +8,33 @@ def packet_scanners(string):
         scanners[int(line[0])] = {}
         scanners[int(line[0])]["direction"] = 1
         scanners[int(line[0])]["len"] = int(line[1])-1
-        scanners[int(line[0])]["pos"] = 0
 
     severity = 0
-    def timestep():
-        for scanner in scanners.values():
-            length = scanner["len"]
-            dr = scanner["direction"]
-            scanner["pos"] += dr
-            if scanner["pos"] in (0, length):
-                scanner["direction"] *= -1
-
+    picosecond = 0
     for i in range(max(scanners)):
+        if i == 0:
+            continue
+        picosecond += 1
         scanner = scanners.get(i)
-        if scanner and scanner["pos"] == 0:
+        if scanner and picosecond % ((scanner["len"])*2) == 0:
             severity += i*(scanner["len"]+1)
-        timestep()
 
     print "Part 1 Solution: %s" % severity
 
-    def reset_scanners():
-        for scanner in scanners.values():
-            scanner["direction"] = 1
-            scanner["pos"] = 0
-
     start_picosecond = -1
     severity = 1
-    reset_scanners()
-    snapshot = deepcopy(scanners)
     while severity != 0:
         if not start_picosecond % 1000:
             print start_picosecond
         start_picosecond += 1
-        scanners = deepcopy(snapshot)
-        timestep()
-        snapshot = deepcopy(scanners)
+        picosecond = start_picosecond
         severity = 0
         for i in range(max(scanners)):
+            picosecond += 1
             scanner = scanners.get(i)
-            if scanner and scanner["pos"] == 0:
+            if scanner and picosecond % ((scanner["len"])*2) == 0:
                 severity = 1
                 break
-            timestep()
 
     print "Part 2 Solution: %s" % start_picosecond
 
