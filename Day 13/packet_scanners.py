ed1b7da173a88deef9b0deadec483fec831a95b8
Michael Witkovsky <michael.witkovsky@gmail.com>
2017-12-21 23:20:14 -0500

    day 13 part 2

--- Day 13/packet_scanners.py
+++ Day 13/packet_scanners.py
@@ -5,34 +5,24 @@ def packet_scanners(string):
     scanners = {}
     for line in string.splitlines():
         line = line.split(": ")
-        scanners[int(line[0])] = {}
-        scanners[int(line[0])]["direction"] = 1
-        scanners[int(line[0])]["len"] = int(line[1])-1
+        scanners[int(line[0])] = int(line[1])-1
 
     severity = 0
-    picosecond = 0
-    for i in range(max(scanners)):
-        if i == 0:
-            continue
-        picosecond += 1
-        scanner = scanners.get(i)
-        if scanner and picosecond % ((scanner["len"])*2) == 0:
-            severity += i*(scanner["len"]+1)
+    for i in scanners:
+        length = scanners[i]
+        if i % (length*2) == 0:
+            severity += i*(length+1)
 
     print "Part 1 Solution: %s" % severity
 
     start_picosecond = -1
     severity = 1
     while severity != 0:
-        if not start_picosecond % 1000:
-            print start_picosecond
         start_picosecond += 1
-        picosecond = start_picosecond
         severity = 0
-        for i in range(max(scanners)):
-            picosecond += 1
-            scanner = scanners.get(i)
-            if scanner and picosecond % ((scanner["len"])*2) == 0:
+        for i in scanners:
+            length = scanners[i]
+            if (start_picosecond+i) % (length*2) == 0:
                 severity = 1
                 break
 
