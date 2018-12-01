9789b65db3b60969968691830abfa4368ceaa873
Michael Witkovsky <michael.witkovsky@gmail.com>
2018-12-01 00:35:07 -0500

    optimize 2018d1

--- 2018/Day1/chronal_calibration.py
+++ 2018/Day1/chronal_calibration.py
@@ -12,21 +12,21 @@ def get_frequencies(string):
 
 def analyize_frequencies(frequencies):
     freq_history = set()
-    total_freq = 0
-    for frequency in get_frequencies(frequencies):
-        total_freq += frequency
-    print ("part 1: {0}".format(total_freq))
-
-    freq_history = set()
-    total_freq = 0
+    first_loop_freq = None
     first_duplicate_freq = None
+    total_freq = 0
     while first_duplicate_freq is None:
         for frequency in get_frequencies(frequencies):
             total_freq += frequency
             if total_freq in freq_history:
                 first_duplicate_freq = total_freq
-                break
+                if first_loop_freq is not None:
+                    break
             freq_history.add(total_freq)
+        if first_loop_freq is None:
+            first_loop_freq = total_freq
+
+    print ("part 1: {0}".format(first_loop_freq))
     print ("part 2: {0}".format(first_duplicate_freq))
 
 
